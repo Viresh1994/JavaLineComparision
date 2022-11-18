9f0ad0d0cd570080c8d90c840b7ee06b014d0f36
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-18 23:34:48 +0530

    ADDED: Code for length of line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wel Come to Line Comparision Computation Program!");
+        double length;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter (x1,y1) co-ordinates");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("Enter (x1,y1) co-ordinates");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        System.out.println("Length of line is:"+length);
     }
 }
